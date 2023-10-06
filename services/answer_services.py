8e5de04018277bd05be59191a5b9bd79b5ad0300
Agustín Montaña <81200417+Agustinm28@users.noreply.github.com>
2023-10-06 11:40:50 -0300

    improve(model): langchain implementation (#1)

--- services/answer_services.py
+++ services/answer_services.py
@@ -1,78 +1,45 @@
 from flask_restx import Namespace, Resource
 from flask import request
-from utils.load_model import load_model, generate_gpu_text
+from utils.load_model import load_model
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 import time
 import copy
 from auth.authentication import *
 
 answer = Namespace('answer', description='Answer related operations')
+llm = None
+template = None
 
 @answer.route('/')
 class Answer(Resource):
     @require_api_key
     def post(self):
-        '''
-        Method to get an answer from the model throug CPU with llama_cpp.
-            - Requires an API key in the request header.
-        '''
         start_time = time.time()
-        try:
-            llm = load_model()
-        except Exception as e:
-            error_message = str(e)
-            return answer.abort(500, error_message)
+        global llm
+        global template
+
+        if not llm:
+            try:
+                llm, template = load_model('Vicuna-13b')
+            except Exception as e:
+                error_message = str(e)
+                return answer.abort(500, error_message)
 
         request_data = request.get_json()
-        prompt = request_data.get("prompt")
+        question = request_data.get("prompt")
 
-        if not prompt:
+        if not question:
             return answer.abort(400, "Question not provided in the request")
 
-        # Response structure compatible with llama-2 and vicuna
-        stream = llm(
-            f"Instruction: {prompt}\nResponse:",
-            max_tokens=10000,
-            # stop=["\n","Q:"], # Stops the generation when prompt finds a Q: or a \n
-            echo=True,
-        )
+        # Create prompt from template
+        ## input_variables reads the variables from the template
+        prompt = PromptTemplate(template=template, input_variables=["prompt"])
+        llm_chain = LLMChain(prompt=prompt, llm=llm)
+        # Run the chain and get the stream
+        stream = llm_chain.run(question)
 
         result = copy.deepcopy(stream)
-
-        end_time = time.time()
-        execution_time = end_time - start_time
-        print(f"Execution time in seconds: {execution_time}")
-
-        return {"result": result}
-    
-@answer.route('/gpu')
-class GPUAnswer(Resource):
-    @require_api_key
-    def post(self):
-        '''
-        Method to get an answer from the model throug GPU with transformers.
-            - Requires an API key in the request header.
-            - Requires a valid GPTQ model.
-        '''
-        start_time = time.time()
-        
-        request_data = request.get_json()
-        prompt = request_data.get("prompt")
-
-        if not prompt:
-            return answer.abort(400, "Question not provided in the request")
-        
-        try:
-            answer = generate_gpu_text(
-                prompt=prompt,
-                temperature=0.7,
-                max_length=2000,
-                do_sample=True
-            )
-        except Exception as e:
-            error_message = str(e)
-            return answer.abort(500, error_message)
-        
-        result = copy.deepcopy(answer)
 
         end_time = time.time()
         execution_time = end_time - start_time
--- utils/load_model.py
+++ utils/load_model.py
@@ -1,14 +1,10 @@
 import os
 import json
 import requests
-from llama_cpp import Llama
+from langchain.llms import LlamaCpp
 from tqdm import tqdm
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from langchain.chains import ConversationChain
-from langchain.chains.conversation.memory import ConversationBufferMemory
-import torch
 
-def load_model():
+def load_model(model_name:str=None):
     '''
     Method to load the model. Gets the model name from config.json.
     '''
@@ -18,14 +14,33 @@ def load_model():
     # Read model name from config.json
     with open('data/config.json', 'r') as f:
         config = json.load(f)
-    model_name = config['model']
+    model_config = config[model_name]
 
-    model_path = f'./models/{model_name}'
+    model_path = f'./models/{model_config["model"]}'
+    n_ctx = model_config['n_ctx']
+    temperature = model_config['temperature']
+    max_tokens = model_config['max_tokens']
+    top_p = model_config['top_p']
+    top_k = model_config['top_k']
+    verbose = model_config['verbose']
+    template = model_config['template']
+    repeat_penalty = model_config['repeat_penalty']
 
-    llm = Llama(model_path=model_path)
+    llm = LlamaCpp(
+        model_path=model_path, 
+        n_ctx=n_ctx,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        top_p=top_p,
+        top_k=top_k,
+        verbose=verbose,
+        repeat_penalty=repeat_penalty,
+        echo=True
+        )
+    
     print('Model loaded')
 
-    return llm
+    return llm, template
 
 def get_models():
     '''
@@ -44,7 +59,7 @@ def get_models():
 
     return models
 
-def set_model(model_name:str):
+def set_model(model_name:str): #! CAMBIAR
     '''
     Method to set the model. Where:
         - model_name: name of the model. You can get the models with get_models()
@@ -59,9 +74,9 @@ def set_model(model_name:str):
         # Write the model name in config.json
         with open('data/config.json', 'r') as f:
             config = json.load(f)
-        config['model'] = model_name
+        config['Vicuna-13b']['model'] = model_name
         with open('data/config.json', 'w') as f:
-            json.dump(config, f)
+            json.dump(config, f, indent=4)
         
         print(f'Model {model_name} set up correctly')
 
@@ -131,109 +146,3 @@ def download_model(model_name:str):
         return f"Model {model_name} downloaded"
     else:
         return f"Model {model_name} download failed"
-    
-def get_size(model_name:str):
-    '''
-    Method to get the size of the model. Where:
-        - model_name: name of the model. You can get the models with see_models()
-    '''
-
-    with open('data/models.json', 'r') as f:
-        models = json.load(f)
-
-    # Check if model exists
-    for model in models:
-        if model_name in models[model]:
-            model_url = models[model][model_name]
-        else:
-            pass
-    
-    # Get the size of the model
-    print(f'Getting size of model {model_name}...')
-    response = requests.get(model_url, stream=True)
-
-    if response.status_code == 200:
-        total_size = int(response.headers.get('content-length', 0))
-        return total_size
-    else:
-        return f"Model {model_name} download failed"
-    
-### GPTQ ADDITIONS WITH TRANSFORMERS ###
-    
-def load_gen_model(path: str, model_name: str):
-    # Carga el modelo y el tokenizer
-    model = AutoModelForCausalLM.from_pretrained(model_name,
-                                                cache_dir=path,
-                                                torch_dtype=torch.float16,
-                                                device_map="auto",
-                                                revision="main",
-                                                #force_download=True, 
-                                                #resume_download=False
-                                                )
-
-    tokenizer = AutoTokenizer.from_pretrained(model_name,
-                                            cache_dir=path,
-                                            use_fast=True)
-    
-    return model, tokenizer
-
-def create_lang_chain(model: AutoModelForCausalLM, buffer: ConversationBufferMemory=None):
-    # Crea una cadena de conversación (con memoria opcional) para el modelo cargado
-    conversation = ConversationChain(
-        llm=model, 
-        verbose=True, 
-        memory=buffer,
-    )
-    
-    return conversation
-
-def generate_gpu_text(prompt: str, previous_dialogue: str = None, temperature: float = 0.7, max_length: int = 2000, do_sample: bool = True):
-    # Reset chache
-    torch.cuda.empty_cache()
-
-    #variable=torch.cuda.memory_summary(device=None, abbreviated=False) para ver como tengo ocupada la memoria de la GPU
-    #print(variable)
-
-    # Ruta personalizada en el disco local E para almacenar los modelos descargados y cachés
-    # ejemplo si queremos ejecutar otro modelo q esta en otra rama revision="gptq-8bit--1g-actorder_True"(modelo descargado pero no funciona)
-    #modelos descargados(se colocan en revision)
-        #"gptq-8bit-128g-actorder_True" no funciona con transformers solo con AutoGPTQ
-        #"gptq-8bit--1g-actorder_True" no funciona con transformers solo con AutoGPTQ
-        #"main" funciona con transformers ya q es ExLLaMa
-    #Path utilizados(se colocan en ra_personalizada)
-        #"TheBloke/vicuna-7B-v1.5-GPTQ" (main,gptq-8bit-128g-actorder_True)
-        #"TheBloke/Llama-2-13B-chat-GPTQ"(main,gptq-8bit-128g-actorder_True,gptq-8bit--1g-actorder_True,gptq-4bit-32g-actorder_True)
-
-    # Load name of the model from config.json
-    with open('data/config.json', 'r') as f:
-        config = json.load(f)
-    model_name = config['model']
-
-    # If model doesnt end with GPTQ, return error
-    if not model_name.endswith('GPTQ'):
-        raise Exception('Model is not GPTQ')
-    
-    # Load model and tokenizer
-    model, tokenizer = load_gen_model('./models', model_name)  
-
-
-    if not previous_dialogue:
-        previous_dialogue = "Esta es la primera escena a generar. Aún no hay contexto."
-
-    prompt_template = f'''[INST] <<SYS>>
-        Sos un guionista con 30 años de experiencia en la escritura de diálogos cinematográficos. Cada conjunto de diálogos tiene un contexto (el diálogo anterior) con el cual debe guardar cierta coherencia y seguir un hilo narrativo. DIÁLOGO PREVIO: {previous_dialogue} #! COREEGIR ---
-        <</SYS>>
-        {prompt}[/INST]
-
-        '''
-
-    print("\n\n*** Generate:")
-
-    input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
-  
-    with torch.no_grad():
-        output = model.generate(input_ids, temperature=temperature, max_length=max_length, do_sample=do_sample)
-
-    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-
-    return generated_text
