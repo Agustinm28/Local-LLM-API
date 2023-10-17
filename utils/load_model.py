954fce35a2090881c74fd2b48e31d2834bc5b0d7
Bruno-Orbelli <orbellibruno@gmail.com>
2023-10-17 15:16:44 +0000

    fix(load_model): removed kv_16 to avoid config error

--- utils/load_model.py
+++ utils/load_model.py
@@ -27,7 +27,6 @@ def load_model():
     verbose = model_config['verbose']
     template = model_config['template']
     repeat_penalty = model_config['repeat_penalty']
-    f16_kv = model_config["f16_kv"]
 
     llm = LlamaCpp(
         model_path=model_path, 
@@ -38,7 +37,6 @@ def load_model():
         top_k=top_k,
         verbose=verbose,
         repeat_penalty=repeat_penalty,
-        f16_kv=f16_kv,
         echo=True
         )
     
